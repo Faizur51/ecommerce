30bade6e202d957a0ab98bd12218f238a2f334f6
Faizur Rahman <faizcse51@gmail.com>
2021-08-10 17:31:14 +0600

    website customize

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+
+use App\Http\Controllers\Frontend\SiteController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[SiteController::class,'index'])->name('index');
+
 
 Route::get('admin/dashboard', function () {
     return view('dashboard');
